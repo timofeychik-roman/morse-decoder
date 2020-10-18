b5ce0872458a03cf283abc5f0203bb6eec0ad6cd
timofeychik-roman <timofeychik.roman@yandex.ru>
2020-10-18 14:57:48 +0300

    init: create decode function

--- src/index.js
+++ src/index.js
@@ -35,10 +35,39 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ':      ' '
 };
 
 function decode(expr) {
     // write your solution here
+    let n = 0;
+    let length = expr.length / 10;
+    let sub = "";
+    let substr = "";
+    let temp = ""
+    let s = "";
+    let morse = [];
+    for (let i = 0; i < length; i++) {
+        sub = expr.slice(n, n + 10);
+        if (sub == "**********") {
+            morse.push(" ");
+            n += 10;
+            continue;
+        }
+        substr = "";
+        for (let j = 0; j < 5; j++) {
+            temp = sub.slice(j * 2, j * 2 + 2);
+            if (temp == "10")
+                substr += ".";
+            if (temp == "11")
+                substr += "-";
+        }
+        morse.push(substr);
+        n += 10;
+    }
+    for (let i = 0; i < morse.length; i++)
+        s += MORSE_TABLE[morse[i]];
+    return s;
 }
 
 module.exports = {
